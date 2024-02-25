65881b98ea3c63bb1da569ffd307c6a6aeee3c88
manhoosbilli1 <shoaib.mustafa7@hotmail.com>
2024-02-25 13:09:57 +0500

    basic ui working

---
+++ components/Btns.tsx
@@ -0,0 +1,45 @@
+import {Text, TouchableOpacity, StyleSheet} from 'react-native';
+import React from 'react';
+
+export default function Btns({text, onclick}) {
+  return (
+    <TouchableOpacity style={styles.roundButton2} onPress={onclick}>
+      <Text style={styles.text}>{text}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: 'darkred',
+    fontWeight: 'bold',
+    justifyContent: 'center',
+    alignContent: 'center',
+  },
+  roundButton1: {
+    width: 100,
+    height: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 100,
+    backgroundColor: 'orange',
+    borderBottomColor: 'red',
+    borderWidth: StyleSheet.hairlineWidth,
+  },
+  roundButton2: {
+    marginTop: 20,
+    width: 150,
+    height: 150,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 100,
+    backgroundColor: 'lightgray',
+  },
+});
