7d922957e4a53e7f9adb3c29f9a443703eed46a7
manhoosbilli1 <shoaib.mustafa7@hotmail.com>
2024-03-09 21:12:08 +0500

    added search book by name function

--- components/screens/Entry.tsx
+++ components/screens/Entry.tsx
@@ -1,5 +1,4 @@
 import React, {useState} from 'react';
-import {PermissionsAndroid} from 'react-native';
 import {
   SafeAreaView,
   View,
@@ -7,6 +6,10 @@ import {
   Alert,
   TextInput,
   Text,
+  TouchableWithoutFeedback,
+  KeyboardAvoidingView,
+  Platform,
+  Keyboard,
 } from 'react-native';
 import Btns from '../Btns';
 import * as config from '../../android/app/google-services.json';
@@ -24,106 +27,177 @@ const firebaseConfig = {
   appId: config.client[0].client_info.mobilesdk_app_id,
 };
 
-firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+  // If not, initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+}
 const reference = database().ref('/');
 
-
-const getBooks = async () => {
+const getBooks = async (documentId: string | undefined) => {
   try {
-    const book = await firestore().collection('books').doc('1').get();
-    if (book.data()?.book_name) {
-      Alert.alert(book.data()!.book_name);
-      console.log(book.data()!.book_name);
+    if (!documentId) {
+      console.log('Document ID is undefined');
+      return;
+    }
+
+    const bookRef = firestore().collection('books').doc(documentId);
+    const bookSnapshot = await bookRef.get();
+
+    if (bookSnapshot.exists) {
+      const bookData = bookSnapshot.data();
+      Alert.alert(bookData?.Book_name);
+      console.log(bookData?.Book_name);
     } else {
-      console.log('no book');
+      console.log('No book found');
     }
   } catch (error) {
     console.log(error);
   }
 };
 
-const searchBook = () => {
-  getBooks();
+const searchBookByCode = async (bookCode: string | undefined) => {
+  try {
+    if (!bookCode) {
+      console.log('Book code is undefined');
+      return;
+    }
+
+    const querySnapshot = await firestore().collection('books').doc(bookCode);
+
+    if (!querySnapshot.empty) {
+      const bookData = querySnapshot.data().get().Book_name;
+      Alert.alert(
+        'Book Details',
+        `Name: ${bookData.Book_name}\nDescription: ${bookData.Book_description}`,
+      );
+    } else {
+      console.log('Book not found');
+      Alert.alert('Book not found');
+    }
+  } catch (error) {
+    console.log('Firestore Query Error:', error);
+  }
 };
 
-/**
- * Displays an alert indicating that a question is being searched for an answer.
- */
-const askQuestion = () => {
-  Alert.alert('Searching for answer...');
+const searchBookByName = async bookName => {
+  try {
+    if (!bookName) {
+      console.log('Book name is undefined');
+      return;
+    }
+
+    const querySnapshot = await firestore().collection('books').get();
+
+    if (!querySnapshot.empty) {
+      const matchingBooks = [];
+
+      querySnapshot.forEach(doc => {
+        const bookData = doc.data();
+        if (bookData.Book_name === bookName) {
+          matchingBooks.push(bookData);
+        }
+      });
+
+      if (matchingBooks.length > 0) {
+        // Display book details in a popup
+        matchingBooks.forEach(bookData => {
+          Alert.alert('Book Details', `Name: ${bookData.Book_name}\n`);
+        });
+      } else {
+        console.log('Book not found');
+        Alert.alert('Book not found');
+      }
+    } else {
+      console.log('No books in the collection');
+      Alert.alert('No books in the collection');
+    }
+  } catch (error) {
+    console.log('Firestore Query Error:', error);
+  }
 };
 
-/**
- * Entry screen component.
- *
- * Renders a screen with a text input to search books,
- * and buttons to navigate to ask a question or search books.
- *
- * Manages local state for the search input text and result.
- */
-const Entry = ({navigation}) => {
-  reference.set({
-    key1: 'value1',
-    key2: 'value2',
-  });
-  const [searchResult, setSearchResult] = useState<string>('');
-  const [searchInput, setSearchInput] = useState<string>('');
+const Entry = () => {
+  const [searchInput, setSearchInput] = useState('');
+  const [bookName, setBookName] = useState('');
+  const [bookCode, setBookCode] = useState('');
+  function askQuestion(): void {
+    throw new Error('Function not implemented.');
+  }
 
   return (
-    <SafeAreaView>
-      <View style={styles.buttonContainer}>
-        <Btns text={'Ask a question'} onclick={askQuestion} />
-        <TextInput
-          style={styles.input}
-          defaultValue="what is the most read book?..."
-          onChangeText={text => setSearchInput(text)}
-        />
-        <Text style={styles.text}>{searchInput}</Text>
-      </View>
-      <View style={styles.buttonContainer}>
-        <Btns text={'Search book'} onclick={searchBook} />
-        <View style={styles.inputContainer}>
-          <TextInput defaultValue="book_name" style={styles.input} />
-          <TextInput defaultValue="Code" style={styles.input} />
-        </View>
-      </View>
+    <SafeAreaView style={styles.container}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <KeyboardAvoidingView
+          style={styles.innerContainer}
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+          <View style={styles.buttonContainer}>
+            <Btns text={'Ask a question'} onclick={askQuestion} />
+            <TextInput
+              style={styles.input}
+              placeholder="What is the most read book?..."
+              onChangeText={text => setSearchInput(text)}
+            />
+            <Text style={styles.text}>{searchInput}</Text>
+          </View>
+          <View style={styles.buttonContainer}>
+            <Btns
+              text={'Search book'}
+              onclick={() => searchBookByName(bookName)}
+            />
+
+            <View style={styles.inputContainer}>
+              <TextInput
+                placeholder="Enter book name"
+                style={styles.input}
+                onChangeText={text => setBookName(text)}
+              />
+              <TextInput
+                placeholder="Enter book code"
+                style={styles.input}
+                onChangeText={text => setBookCode(text)}
+              />
+            </View>
+          </View>
+        </KeyboardAvoidingView>
+      </TouchableWithoutFeedback>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  main: {
+  container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  innerContainer: {
+    flex: 1,
+    width: '80%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    marginBottom: 20,
+    width: '100%',
+    alignItems: 'center',
   },
   inputContainer: {
     flexDirection: 'row',
-    flexWrap: 'wrap',
-    alignContent: 'center',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 'auto',
-    width: 'auto',
-  },
-  buttonContainer: {
-    alignContent: 'center',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 'auto',
+    justifyContent: 'space-between',
     width: '100%',
   },
   input: {
     height: 40,
-    width: '80%',
+    width: '48%',
     borderWidth: 2,
     borderColor: 'black',
-    margin: 10,
-    color: 'black',
+    marginVertical: 10,
+    paddingHorizontal: 10,
   },
   text: {
     color: 'darkred',
     fontWeight: 'bold',
-    justifyContent: 'center',
-    alignContent: 'center',
+    marginTop: 10,
   },
 });
 
